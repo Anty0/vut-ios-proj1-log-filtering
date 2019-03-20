bc76c5d8cd765b7e24dafb464d77b6e789dad181
Jiří Kuchyňka (Anty) <anty150@gmail.com>
2019-03-20 19:31:15 +0100

    reworked for speed up;

--- test/test.sh
+++ test/test.sh
@@ -1,14 +1,21 @@
 #!/bin/zsh
 
-# TODO: dash
 call_wana() {
-    printf "wana %s\\n" "$1" >&2
-    cd ../logs && eval "../wana $1"
+    printf "%s wana %s\\n" "$1" "$2" >&2
+    cd ../logs && eval "$1 ../wana $2"
 }
 
 for file in ./args/*; do
     file_name=${file##*/}
     printf "Test %s\\n" "$file_name"
     args="$( cat "$file" )"
-    diff <( call_wana "$args" ) "./out/$file_name"
+    if command -v dash >/dev/null; then
+        diff <( call_wana "dash" "$args" ) "./out/$file_name"
+    fi
+    if command -v bash >/dev/null; then
+        diff <( call_wana "bash" "$args" ) "./out/$file_name"
+    fi
+    if command -v zsh >/dev/null; then
+        diff <( call_wana "zsh" "$args" ) "./out/$file_name"
+    fi
 done
