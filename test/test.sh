d3d72f3bff9d4cb8aa9a8cb2b16e4dd0180993ab
Jiří Kuchyňka (Anty) <anty150@gmail.com>
2019-03-26 13:39:09 +0100

    update; finish changes;

--- test/test.sh
+++ test/test.sh
@@ -9,13 +9,9 @@ for file in ./args/*; do
     file_name=${file##*/}
     printf "Test %s\\n" "$file_name"
     args="$( cat "$file" )"
-    if command -v dash >/dev/null; then
-        diff <( call_wana "dash" "$args" ) "./out/$file_name"
-    fi
-    if command -v bash >/dev/null; then
-        diff <( call_wana "bash" "$args" ) "./out/$file_name"
-    fi
-    if command -v zsh >/dev/null; then
-        diff <( call_wana "zsh" "$args" ) "./out/$file_name"
-    fi
+    for shell in "$@"; do
+        if command -v "$shell" >/dev/null; then
+            diff <( call_wana "$shell" "$args" ) "./out/$file_name"
+        fi
+    done
 done
